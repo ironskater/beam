1914659fec2e17c622f5cedfdd6a55363f7ce11f
Hyde <hyde_liao@acbel.com>
2019-05-20 10:23:30 +0800

    [tmuxbench] add scirpt

---
+++ tmuxbench.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo -ne "\033]0;$1\007";tmux
+
+#SESSIONNAME="script"
+#tmux has-session -t $SESSIONNAME &> /dev/null
+
+#if [ $? != 0 ]
+ #then
+    #tmux new-session -s $SESSIONNAME -n script -d
+    #tmux send-keys -t $SESSIONNAME "~/bin/script" C-m
+#fi
+
+#tmux attach -t $SESSIONNAME
