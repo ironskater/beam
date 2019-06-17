ea3dfd0f2523888a503500e5ca378fe5fe747f32
Hyde Liao <kcritkernel@gmail.com>
2019-06-18 01:38:17 +0800

    [Update] update mvnCreate and mvnExec

--- mvnCreate.sh
+++ mvnCreate.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-mvn archetype:generate 	-DgroupId=$1 \
-						-DartifactId=$2 \
-						-DarchetypeArtifactId=maven-archetype-quickstart \
-						-DinteractiveMode=false
+# DartifactId為專案資料夾名稱, DgroupId為package, 預設產生的class為App.java
+mvn archetype:generate -DartifactId=$1 -DgroupId=$2 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
--- mvnExec.sh
+++ mvnExec.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn clean install; mvn exec:java -Dexec.mainClass=com.fujitsu.smms.othersystem.SJB101010 -Dexec.args="SJB101010 /smms/apl/lock/ SJB101010Receive.properties 41100"
+mvn clean install; mvn exec:java -Dexec.mainClass=$1
